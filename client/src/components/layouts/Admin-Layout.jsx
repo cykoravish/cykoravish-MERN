9d3049d16c4316b669a92ab1b6173051acda15b6
Ravish Bisht <ravishbisht85@gmail.com>
2024-01-19 10:50:32 +0530

    admin pannel

---
+++ client/src/components/layouts/Admin-Layout.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { FaUser, FaHome, FaRegListAlt } from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
+
+export const AdminLayout = () => {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/admin/users">
+                  <FaUser />
+                  Users
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/admin/contacts">
+                  <FaMessage />
+                  Contacts
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/service">
+                  <FaRegListAlt />
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/">
+                  <FaHome />
+                  Home
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <Outlet />
+    </>
+  );
+};
---
+++ client/src/pages/Admin-Contacts.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AdminContacts = () => {
+  return <h1>Admin-contacts</h1>;
+};
