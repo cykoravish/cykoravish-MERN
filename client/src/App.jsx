525fd1db2839a85340108b7502f9443167be254c
Ravish Bisht <ravishbisht85@gmail.com>
2024-01-19 11:53:54 +0530

    admin add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import { Contact } from "./pages/Contact";
-import {Service} from "./pages/Service";
+import { Service } from "./pages/Service";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import Error from "./pages/Error";
 import Footer from "./components/Footer/Footer";
 import { Logout } from "./pages/Logout";
+import { AdminLayout } from "./components/layouts/Admin-Layout";
+import { AdminUsers } from "./pages/Admin-Users";
+import { AdminContacts } from "./pages/Admin-Contacts";
 
 const App = () => {
   return (
@@ -25,6 +28,10 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="*" element={<Error />} />
+          <Route path="/admin" element={<AdminLayout/>}>
+            <Route path="users" element={<AdminUsers />} />
+            <Route path="contacts" element={<AdminContacts />} />
+          </Route>
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ client/src/pages/Admin-Users.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AdminUsers = () => {
+  return <h1>Admin-Users</h1>;
+};
